914c7c7319f8b9c9661f4da242b328e3cd1a67ad
alabhyamobar <alabhyamobar50@gmail.com>
2025-09-05 14:14:03 +0530

    creating entry point

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from "path";
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
-  base: '/betia/'
+  base: '/betia/',
+   build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        celebrate: resolve(__dirname, "celebrate.html"),
+      },
+    },
+  },
 })
